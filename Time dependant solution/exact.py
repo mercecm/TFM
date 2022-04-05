92a4cbebd6688e35b06814f26b0e0e4b979fe9e3
mercecm <96746157+mercecm@users.noreply.github.com>
2022-04-05 16:48:03 +0200

    05/04

--- Time dependant solution/exact.py
+++ Time dependant solution/exact.py
@@ -2,19 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import erfc
 
-#mu = 0.01
-#cb = 1.0
 t0 = 0.0001
 t1 = 0.0020
 t2 = 0.0040
-t3 = 1.0 #estacionari
+
 
 L = 0.05
 w = 0.16E-6
 D = 4.3E-11
 mu = D/(L*w)
-
-x = np.linspace(0,1,1000)
 
 def c_exact(x,t,mu):
   c = (1-np.exp(-x/mu)) - (1/2)*(erfc((x+t)/(np.sqrt(4*mu*t))) - np.exp(-x/mu)*(2-erfc((x-t)/(np.sqrt(4*mu*t)))))
@@ -38,30 +34,70 @@ def flux_surf(t,mu):
   f = 1-(1/2)*erfc(np.sqrt(t/(4*mu)))
   return f
 
-t = np.linspace(0,0.05,100)
+x1 = 0.005
+x2 = 0.03 
+xe = np.linspace(0.0,1.0,1000)
+xe = xe.astype(float)
+te = np.linspace(0.0,0.05,100)
+
 fig = plt.figure ( )
 ax = plt.subplot ( 111 )
-plt.plot(t,flux_surf(t,mu))
+plt.plot(te,flux_surf(te,mu), label = 'x = 0.0')
+plt.plot(te,flux_exact(x1,te,mu), label = 'x = %.3f' %x1)
+plt.plot(te,flux_exact(x2,te,mu), label = 'x = %.2f' %x2)
 ax.legend ( )
+plt.xlabel('Time')
+plt.ylabel('Flux')
 ax.grid ( True )
-plt.title ( 'Flux in surface x=0' )
-filename = ( 'flux_surf.png' )
+plt.title ( 'Exact flux in surface' )
+filename = ( 'flux_exact_3surf.png' )
 plt.savefig ( filename )
 print ( '  Graphics saved as "%s"' % ( filename ) )
-plt.show()
-#plt.close ( )
+#plt.show()
+plt.close ( )
 
 #fig = plt.figure ( )
 #ax = plt.subplot ( 111 )
-#plt.plot(x,flux_exact(x,t0,mu), label = 't = %f' %t0)
-#plt.plot(x,flux_exact(x,t1,mu), label = 't = %f' %t1)
-#plt.plot(x,flux_exact(x,t2,mu), label = 't = %f' %t2)
-#plt.plot(x,flux_stationary(x,mu), label = 'stationary solution')
+#plt.plot(te,flux_exact(x1,te,mu))
 #ax.legend ( )
 #ax.grid ( True )
-#plt.title ( 'exact solution at time %f and %f' % ( t1,t2 ) )
-#filename = ( 'flux_exact_t%f.png' % ( t1 ) )
+#plt.title ( 'Flux in surface x=%.2f' %x1 )
+#filename = ( 'flux_surf_x1.png' )
 #plt.savefig ( filename )
 #print ( '  Graphics saved as "%s"' % ( filename ) )
 #plt.show()
 #plt.close ( )
+
+fig = plt.figure ( )
+ax = plt.subplot ( 111 )
+plt.plot(xe,flux_exact(xe,t0,mu), label = 't = %f' %t0)
+plt.plot(xe,flux_exact(xe,t1,mu), label = 't = %f' %t1)
+plt.plot(xe,flux_exact(xe,t2,mu), label = 't = %f' %t2)
+plt.plot(xe,flux_stationary(xe,mu), label = 'stationary solution')
+ax.legend ( )
+plt.xlabel('Position')
+plt.ylabel('Flux')
+ax.grid ( True )
+plt.title ( 'Exact solution for flux' )
+filename = ( 'flux_exact.png' )
+plt.savefig ( filename )
+print ( '  Graphics saved as "%s"' % ( filename ) )
+#plt.show()
+plt.close ( )
+
+fig = plt.figure ( )
+ax = plt.subplot ( 111 )
+plt.plot(xe,c_exact(xe,t0,mu), label = 't = %f' %t0)
+plt.plot(xe,c_exact(xe,t1,mu), label = 't = %f' %t1)
+plt.plot(xe,c_exact(xe,t2,mu), label = 't = %f' %t2)
+plt.plot(xe,c_stationary(xe,mu), label = 'stationary solution')
+ax.legend ( )
+plt.xlabel('Position')
+plt.ylabel('Concentration')
+ax.grid ( True )
+plt.title ( 'Exact solution for concentration' )
+filename = ( 'concentration_exact.png' )
+plt.savefig ( filename )
+print ( '  Graphics saved as "%s"' % ( filename ) )
+#plt.show()
+plt.close ( )
--- Time dependant solution/readme.txt
+++ Time dependant solution/readme.txt
@@ -2,6 +2,8 @@
 
 tDependant - té definides les solucions exactes correctament, però dóna error quan es fa el plot. La concentració i el flux al temps final dónen força bé, però el flux a la superfície no és el que hauria de ser. Hi ha els resultats obtinguts per dues malles diferents amb dt = 0.001.
 
-flux_surf_grid50_x0 és el nou resultat pel flux a la superfície
+tDependant2 - no té definides les solucions exactes. Plot del flux en 3 superfícies diferents i labels als eixos.
 
-exact - ha generat les imatges flux_surf.png, flux_exact.png i concentration_exact.png amb les solucions exactes. 
+flux_surf_grid50_x0 és el nou resultat pel flux a la superfície; Flux_surf_grid50 té el flux a 3 superfícies diferents
+
+exact - ha generat les imatges flux_exact_3surf.png, flux_exact.png i concentration_exact.png amb les solucions exactes. 
